512bd7794792f64335a277944d0a44d059cec679
homePc\Ebbe Vang <ev@evang.dk>
2018-05-06 11:25:13 +0200

    adds player with movement and collision detection

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float Speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.DownArrow))
+	    {
+            //Debug.Log("make sure this is happening");
+            transform.Translate(Vector3.down * Speed * Time.deltaTime);
+        }
+
+	    if (Input.GetKey(KeyCode.UpArrow))
+	    {
+	        transform.Translate(Vector3.up * Speed * Time.deltaTime);
+        }
+
+	    if (Input.GetKey(KeyCode.LeftArrow))
+	    {
+	        transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+
+	    if (Input.GetKey(KeyCode.RightArrow))
+	    {
+	        transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        }
+
+	    if (Input.GetKey(KeyCode.Space))
+	    {
+            // shoot / spawn bullet
+	    }
+	}
+}
